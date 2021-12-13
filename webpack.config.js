c90508bc8f3eba2ec9ddd08c72e0b8f44e73e8d3
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-14 05:10:32 +0530

    The extract text plugin completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
   entry: "./src/index.js",
@@ -13,11 +14,15 @@ const config = {
         test: /\.js$/,
       },
       {
-        use: ["style-loader", "css-loader"], // order must be same
         test: /\.css$/i,
+        loader: ExtractTextPlugin.extract({
+          loader: "css-loader",
+        }),
+        // use: ["style-loader", "css-loader"], // order must be same
       },
     ],
   },
+  plugins: [new ExtractTextPlugin("style.css")],
 };
 
 module.exports = config;
