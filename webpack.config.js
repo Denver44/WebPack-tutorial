8b67765f106874662d063e3e947324001af0624a
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-14 05:21:05 +0530

    image loader config file updated

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,16 @@ const config = {
         }),
         // use: ["style-loader", "css-loader"], // order must be same
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 40000 },
+          },
+          "image-webpack-loader",
+        ],
+      },
     ],
   },
   plugins: [new ExtractTextPlugin("style.css")],
